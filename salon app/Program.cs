019e0658ab57b967a5902f753b4c69b75247e39c
SithijaDK <sithija.pabasara@gmail.com>
2025-11-21 15:57:55 +1300

    sithija - made ui changes in payment (form6)

--- salon app/Program.cs
+++ salon app/Program.cs
@@ -16,7 +16,7 @@ namespace salon_app
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Form6());
         }
     }
 }
