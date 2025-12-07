f79571264326144c2ffd64976ab5c6c525ea9d2e
SithijaDK <sithija.pabasara@gmail.com>
2025-12-07 19:57:37 +1300

    sithija- make changes in program.cs to load in login page. final push before merging with master

--- salon app/Program.cs
+++ salon app/Program.cs
@@ -16,7 +16,7 @@ namespace salon_app
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form6());
+            Application.Run(new Login());
         }
     }
 }
